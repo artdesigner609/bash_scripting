f4def75546b149f9fb2fbf66bfe4026793b7c364
AhsanKhan <ahsankhan609@gmail.com>
2026-06-28 18:30:40 +0500

    Add conditional check for count in conditional_statements.sh. Introduced a new section to evaluate if count equals 10, enhancing the script's demonstration of conditional statements in Bash.

--- scripts/conditional_statements.sh
+++ scripts/conditional_statements.sh
@@ -20,3 +20,13 @@ else
 fi
 
 echo "--------------------------------"
+
+count=10
+
+if [ $count -eq 10 ]; then
+    echo "Count is 10"
+else
+    echo "Count is not 10"
+fi
+
+echo "--------------------------------"
