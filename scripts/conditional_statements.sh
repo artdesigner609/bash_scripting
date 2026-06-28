129f51e4f707698fe2db88682916357258ab8ce9
AhsanKhan <ahsankhan609@gmail.com>
2026-06-28 15:21:08 +0500

    Add new script 'conditional_statements.sh' demonstrating the use of conditional statements in Bash. The script includes a lottery simulation and user interaction to check preferences for Bash, enhancing user engagement and understanding of conditional logic.

---
+++ scripts/conditional_statements.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+random_number=$((RANDOM % 100))
+
+if [ "$random_number" -gt 50 ]; then
+    echo "You won a lottery of $random_number."
+else
+    echo "You lost the lottery. The number was $random_number."
+fi
+
+echo "--------------------------------"
+
+echo "Do you Like BASH? (y/n)"
+read like_bash
+
+if [ "$like_bash" = "y" ]; then
+    echo "You like BASH."
+else
+    echo "You don't like BASH. But Why???"
+fi
+
+echo "--------------------------------"
