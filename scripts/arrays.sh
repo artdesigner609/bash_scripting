eb2e3f21c51b0932ba64c5b0d89234257cc69aed
AhsanKhan <ahsankhan609@gmail.com>
2026-06-28 17:59:00 +0500

    Add new script arrays.sh to demonstrate array declaration and indexing in Bash. The script includes examples for printing array elements, enhancing practical understanding of array usage in Bash scripting.

---
+++ scripts/arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# declare an array of cars
+cars=("car" "train" "bike" "bus")
+
+echo "${cars[0]}" # print the first item in the array
+echo "${cars[1]}" # print the second item in the array
+echo "${cars[2]}" # print the third item in the array
+echo "${cars[3]}" # print the fourth item in the array
